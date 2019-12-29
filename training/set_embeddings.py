ecedca3e487ff9ad1bb56402204b55dae8cbac1b
ayls <ales.daneu@gmail.com>
2019-12-29 13:24:22 +0100

    Save embedding vectors and test face detection

--- training/set_embeddings.py
+++ training/set_embeddings.py
@@ -11,32 +11,42 @@ workers = 0 if os.name == 'nt' else 4
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print('Running on device: {}'.format(device))
 
+# face detection pipeline
 mtcnn = MTCNN(
-    image_size=160, margin=0, min_face_size=20,
-    thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
-    device=device
+  image_size=160, margin=0, min_face_size=20,
+  thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+  device=device
 )
 
+# facenet pipeline
 resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
 
+# read images
 def collate_fn(x):
-    return x[0]
+  return x[0]
 
-dataset = datasets.ImageFolder('./test_images')
+dataset = datasets.ImageFolder('./training_images')
 dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
 loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
 
+# run mtcnn pipeline
 aligned = []
 names = []
 for x, y in loader:
-    x_aligned, prob = mtcnn(x, return_prob=True)
-    if x_aligned is not None:
-        print('Face detected with probability: {:8f}'.format(prob))
-        aligned.append(x_aligned)
-        names.append(dataset.idx_to_class[y])
+  x_aligned, prob = mtcnn(x, return_prob=True)
+  if x_aligned is not None:
+    print('Face detected with probability: {:8f}'.format(prob))
+    aligned.append(x_aligned)
+    names.append(dataset.idx_to_class[y])
 
+# run facenet
 aligned = torch.stack(aligned).to(device)
 embeddings = resnet(aligned).detach().cpu()
 
+# display results
 dists = [[(e1 - e2).norm().item() for e2 in embeddings] for e1 in embeddings]
 print(pd.DataFrame(dists, columns=names, index=names))
+
+# save names embeddings
+np.save('names.npy', names)
+torch.save(embeddings, 'embeddings.pt')
---
+++ training/test_detection.py
@@ -0,0 +1,62 @@
+from facenet_pytorch import MTCNN, InceptionResnetV1
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets
+import numpy as np
+import os
+
+workers = 0 if os.name == 'nt' else 4
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print('Running on device: {}'.format(device))
+
+stored_embeddings = torch.load('embeddings.pt')
+stored_names = np.load('names.npy')
+
+# face detection pipeline
+mtcnn = MTCNN(
+  image_size=160, margin=0, min_face_size=20,
+  thresholds=[0.6, 0.7, 0.7], factor=0.709, post_process=True,
+  device=device
+)
+
+# facenet pipeline
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+
+# read images
+def collate_fn(x):
+  return x[0]
+
+dataset = datasets.ImageFolder('./test_images')
+dataset.idx_to_class = {i:c for c, i in dataset.class_to_idx.items()}
+loader = DataLoader(dataset, collate_fn=collate_fn, num_workers=workers)
+
+# run mtcnn pipeline
+aligned = []
+for x, y in loader:
+  x_aligned, prob = mtcnn(x, return_prob=True)
+  if x_aligned is not None:
+    print('Face detected with probability: {:8f}'.format(prob))
+    aligned.append(x_aligned)
+
+# run facenet
+aligned = torch.stack(aligned).to(device)
+embeddings = resnet(aligned).detach().cpu()
+
+# calculate distances and display detection results
+results = []
+for idx, e1 in enumerate(embeddings):
+  minDiff = 100
+  minDiffIdx = -1
+  for e2 in stored_embeddings:
+    diff = (e1 - e2).norm().item()
+    if diff < minDiff and diff < 0.75:
+      minDiff = diff
+      minDiffIdx = idx
+  if minDiffIdx >= 0:
+    results.append(stored_names[minDiffIdx])
+  else:
+    results.append('Unknown')    
+
+print(results)
+
